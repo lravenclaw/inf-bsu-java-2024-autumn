c826989e38f96e9e81398ed32686d4408febb519
user <happy@mail.com>
2024-10-20 10:44:53 +0300

    lab6 - lab4 additional task

--- lab4/src/main/java/Person.java
+++ lab4/src/main/java/Person.java
@@ -1,6 +1,6 @@
 package solution;
 
-import java.util.Objects;
+import java.util.Comparator;
 
 public class Person implements Comparable<Person> {
     public int id;
@@ -9,8 +9,29 @@ public class Person implements Comparable<Person> {
     public String name;
     public String surname;
 
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
     @Override
     public int compareTo(Person other) {
-        return Integer.compare(this.id, other.id);
+
+        return Comparator.comparing(Person::getId)
+                .thenComparing(Person::getName)
+                .thenComparing(Person::getSurname)
+                .thenComparing(Person::getAge)
+                .compare(this, other);
     }
 }
